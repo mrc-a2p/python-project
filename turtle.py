b532bd2c40262430ce476b8482925c8a8601cbba
Marc Araujo <mrc.a2p@gmail.com>
2019-08-12 19:15:31 +0200

    tortuga a medias

---
+++ turtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen() #renderiza una ventana
+david = turtle.Turtle()
+
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+
+turtle.mainloop()
---
+++ variables.py
@@ -0,0 +1,4 @@
+#name = str (raw_input('cuál es tu nombre?'))
+#print ('hola, ' + name)
+name = str(input('¿Cuál es tu nombre?'))
+print('Hola, ' + name + '!')
