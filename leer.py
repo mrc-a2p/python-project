6e96f64a94d92a320fa9f5bd507459071524b75e
Marc Araujo <mrc.a2p@gmail.com>
2019-09-01 19:06:12 +0200

    buscador de palabras

---
+++ leer.py
@@ -0,0 +1,19 @@
+def run():
+    counter = 0
+    with open("alex.txt") as f:
+        for line in f:
+            counter += line.count("Beatriz") 
+    print("Beatriz se encuentra {} en el texto".format(counter))   
+
+
+
+if __name__ == '__main__':
+    run()
+
+
+
+
+
+
+
+
